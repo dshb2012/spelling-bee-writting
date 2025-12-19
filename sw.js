e9a310d3fc5beadbe161efd474c210b9b162bc36
dshb2012 <shahabdella20@gmail.com>
2025-12-19 09:58:06 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = "reading-app-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
